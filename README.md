11e097e9de47d43a6591de17b10a1a71ff2c65c0
Sam Selikoff <sam.selikoff@gmail.com>
2013-08-28 03:15:40 -0400

    Adds barchart, package.json, license

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ d3.chart.barchart
 =================
 
 A simple bar chart built with d3.chart
+
+### Sample use
+
+    var barChart = d3.select("#bar-chart")
+      .append("svg")
+      .chart("BarChart")
+      .width(200)
+      .height(150);
+
+    barChart.draw([
+      {name: 'a', value: 4},
+      {name: 'b', value: 16},
+      {name: 'c', value: 19},
+      {name: 'd', value: 8},
+      {name: 'e', value: 6},
+    ]);
---
+++ d3.chart.barchart.js
@@ -0,0 +1,79 @@
+(function() {
+  d3.chart('BarChart', {
+    initialize : function() {
+      this._margin = {top: 40, right: 20, bottom: 40, left: 60};
+      this.xScale  = d3.scale.ordinal();
+      this.yScale  = d3.scale.linear();
+      this._color  = d3.scale.category10();
+
+      this._width = this._width || this.base.attr('width') || 200;
+      this._height = this._height || this.base.attr('height') || 100;
+
+      var barsLayerBase = this.base.append('g')
+        .classed('bars', true);
+
+      this.layer('bars', barsLayerBase, {
+
+        dataBind: function(data) {
+          var chart = this.chart();
+
+          // Update the x-scale.
+          chart.xScale.domain(data.map(function(d) { return d.name; }));
+
+          // Update the y-scale.
+          chart.yScale.domain([
+            d3.min(data, function(d) { return d3.min([d.value, 0]); }),
+            d3.max(data, function(d) { return d.value; })
+          ]);
+
+          return this.selectAll('.bar')
+            .data(data);
+        },
+
+        insert: function() {
+          var chart = this.chart();
+
+          // Append the bars
+          return this.append('rect')
+            .attr('fill', 'blue')
+            .attr('width', chart.xScale.rangeBand());
+        },
+
+        events: {
+          enter: function() {
+            var chart = this.chart();
+
+            return this
+              .attr('x', function(d) { return chart.xScale(d.name); })
+              .attr('y', function(d) { return chart.yScale(d3.max([0, d.value])); })
+              .attr('height', function(d) { return Math.abs(chart.yScale(d.value) - chart.yScale(0)); })
+              .attr('fill', function(d) {return chart._color(d.name);})
+              ;
+          }
+        }
+
+      });
+    },
+
+    width: function(newWidth) {
+      if (arguments.length === 0) {
+          return this._width;
+      }
+      this._width = newWidth;
+      this.base.attr('width', this._width);
+      this.xScale.rangeRoundBands([0, newWidth], 0.1);
+      return this;
+    },
+    
+    height: function(newHeight) {
+      if (arguments.length === 0) {
+          return this._height;
+      }
+      this._height = newHeight;
+      this.base.attr('height', this._height);
+      this.yScale.range([newHeight, 0]);
+      return this;
+    },
+
+  });
+}());
